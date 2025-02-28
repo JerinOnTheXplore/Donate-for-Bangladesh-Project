e0b7d366a360faa26e1254e64bfc466d35d6c98a
JerinXplore <jerinjerin101325@gmail.com>
2025-02-28 19:23:10 +0600

    added more functional buttons

--- script/donation.js
+++ script/donation.js
@@ -4,10 +4,15 @@ document.getElementById('donate-1').addEventListener('click', function(event){
     const convertedMainBalance = parseFloat(mainBalance);
     const payForNoakhali = document.getElementById('amount-1').value;
     const convertedPayForNoakhali = parseFloat(payForNoakhali);
-    if(convertedPayForNoakhali <= convertedMainBalance && convertedPayForNoakhali > 0){
+    const currentDonation = document.getElementById('display-donation-1').innerText;
+    const convertedCurrentDonation = parseFloat(currentDonation);
+    if(convertedPayForNoakhali <= convertedMainBalance && convertedPayForNoakhali >= 0){
         const deduction = convertedMainBalance - convertedPayForNoakhali;
         document.getElementById('main-balance').innerText = deduction;
-        document.getElementById('display-donation-1').innerText = convertedPayForNoakhali;
+        
+        const currentDonateAmount = convertedPayForNoakhali + convertedCurrentDonation;
+        document.getElementById('display-donation-1').innerText = currentDonateAmount;
+        
     }
     else{
         alert('Invalid input');
@@ -19,10 +24,14 @@ document.getElementById('donate-2').addEventListener('click', function(event){
     const convertedMainBalance = parseFloat(mainBalance);
     const payForFeni = document.getElementById('amount-2').value;
     const convertedPayForFeni = parseFloat(payForFeni);
-    if(convertedPayForFeni <= convertedMainBalance && convertedPayForFeni > 0){
+    const currentDonation = document.getElementById('display-donation-2').innerText;
+    const convertedCurrentDonation = parseFloat(currentDonation);
+    if(convertedPayForFeni <= convertedMainBalance && convertedPayForFeni >= 0){
         const deduction = convertedMainBalance - convertedPayForFeni;
         document.getElementById('main-balance').innerText = deduction;
-        document.getElementById('display-donation-2').innerText = convertedPayForFeni;
+        
+        const currentDonateAmount = convertedPayForFeni + convertedCurrentDonation;
+        document.getElementById('display-donation-2').innerText = currentDonateAmount;
     }
     else{
         alert('Invalid input');
@@ -34,10 +43,14 @@ document.getElementById('donate-3').addEventListener('click', function(event){
     const convertedMainBalance = parseFloat(mainBalance);
     const aidForInjured = document.getElementById('amount-3').value;
     const convertedAidForInjured= parseFloat(aidForInjured);
+    const currentDonation = document.getElementById('display-donation-3').innerText;
+    const convertedCurrentDonation = parseFloat(currentDonation);
     if(convertedAidForInjured <= convertedMainBalance && convertedAidForInjured > 0){
         const deduction = convertedMainBalance - convertedAidForInjured;
         document.getElementById('main-balance').innerText = deduction;
-        document.getElementById('display-donation-3').innerText = convertedAidForInjured;
+        
+        const currentDonateAmount = convertedAidForInjured + convertedCurrentDonation;
+        document.getElementById('display-donation-3').innerText = currentDonateAmount;
     }
     else{
         alert('Invalid input');
